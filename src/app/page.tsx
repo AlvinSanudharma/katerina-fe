ebb4446b5685d72b8395509bac93d56b9778d09c
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-09-03 11:06:45 +0800

    Sale Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import LogoKaterina from "@/assets/image/logo-katerina.svg";
 import FlagId from "@/assets/image/flag-id.svg";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
@@ -18,6 +19,46 @@ export default function Home() {
           </button>
         </span>
       </header>
+      <section className="relative">
+        <Slider
+          spaceBetween={20}
+          swiperClassName="!h-[180px] !px-4"
+          swiperSliderClassName="!max-w-xs"
+        >
+          <div className="h-full rounded-3xl overflow-hidden relative border">
+            <figure className="w-full h-full absolute">
+              <img
+                className="w-full h-full object-cover object-center"
+                src="/images/slide1.png"
+                alt=""
+              />
+            </figure>
+            <div className="absolute inset-0 bg-gradient-to-r from-black to-black/0"></div>
+            <div className="absolute left-0 bottom-0 top-0 pl-4 w-32 flex flex-col justify-center font-bold">
+              <span className="text-white">Sale</span>
+              <span className="text-color1 text-4xl">50%</span>
+              <span className="text-white">Potongan</span>
+            </div>
+            {/* <a href="details.html" className="absolute inset-0"></a> */}
+          </div>
+          <div className="h-full rounded-3xl overflow-hidden relative border">
+            <figure className="w-full h-full absolute">
+              <img
+                className="w-full h-full object-cover object-center"
+                src="/images/slide1.png"
+                alt=""
+              />
+            </figure>
+            <div className="absolute inset-0 bg-gradient-to-r from-black to-black/0"></div>
+            <div className="absolute left-0 bottom-0 top-0 pl-4 w-32 flex flex-col justify-center font-bold">
+              <span className="text-white">Sale</span>
+              <span className="text-color1 text-4xl">50%</span>
+              <span className="text-white">Potongan</span>
+            </div>
+            {/* <a href="details.html" className="absolute inset-0"></a> */}
+          </div>
+        </Slider>
+      </section>
     </>
   );
 }
