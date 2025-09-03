4637db0434eb792a1bf8ab73746ae0919d838453
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-09-03 10:49:32 +0800

    Slider Component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,56 @@
+import { Children, ReactNode } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import { A11y, Navigation, Pagination } from "swiper/modules";
+
+type Props = {
+  children: ReactNode;
+  spaceBetween: number;
+  swiperSliderClassName?: string;
+  swiperClassName?: string;
+  hasPagination?: boolean;
+};
+
+function Slider({
+  children,
+  spaceBetween,
+  swiperSliderClassName,
+  swiperClassName,
+  hasPagination,
+}: Props) {
+  let modules = [Navigation, A11y];
+
+  if (!!hasPagination) {
+    modules.push(Pagination);
+  }
+
+  return (
+    <Swiper
+      loop={true}
+      slidesPerView="auto"
+      spaceBetween={spaceBetween}
+      modules={modules}
+      className={swiperClassName}
+      maxBackfaceHiddenSlides={10}
+      pagination={
+        hasPagination
+          ? {
+              clickable: true,
+            }
+          : false
+      }
+    >
+      {Children.toArray(children).map((slide: any) => {
+        return (
+          <SwiperSlide className={swiperSliderClassName} key={slide.key}>
+            {slide}
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
+
+export default Slider;
